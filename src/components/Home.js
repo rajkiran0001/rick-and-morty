8657e96661dde9b21c3a4238c60b8b70526487a7
rajkiran0001 <rajkiran.one@gmail.com>
2020-05-10 21:18:40 +0200

    add bootstrap and display characters

--- src/components/Home.js
+++ src/components/Home.js
@@ -13,9 +13,28 @@ function Home() {
         console.log(err);
       });
   }, []);
-  console.log(characters);
 
-  return <div></div>;
+  return (
+    <>
+      <div className="container">
+        <div className="row">
+          {characters.map((character, id) => (
+            <div key={id}>
+              <div>
+                <p>
+                  <img src={character.image} alt={character.name} />
+                </p>
+                <p>name: {character.name}</p>
+                <p>species: {character.species}</p>
+                <p>status: {character.status}</p>
+                <p>created: {character.created.slice(0, 10)}</p>
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default Home;
